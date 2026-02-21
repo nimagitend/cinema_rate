9926c5ecf791180ca527d3ed3a19ef6e159a7fd7
Nima <nima90kh@gmail.com>
2026-02-21 11:03:58 +0330

    Fix pages not loading on web

--- core/views.py
+++ core/views.py
@@ -1,4 +1,5 @@
 from django.contrib import messages
+from django.contrib.auth import login
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.views import LoginView, LogoutView
 from django.db import connection
